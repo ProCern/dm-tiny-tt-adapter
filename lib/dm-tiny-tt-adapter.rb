60bf83177b6d3468c17aabdc8e736332b76cf5df
Paul Sadauskas <psadauskas@gmail.com>
2009-03-31 17:08:01 -0600

    I think its done

--- lib/dm-tiny-tt-adapter.rb
+++ lib/dm-tiny-tt-adapter.rb
@@ -18,24 +18,66 @@ module DataMapper::Adapters
     end
 
     def read(query)
-      model = query.model
+      metric_uuid, start_time, end_time = parse_query(query)
 
       db do |db|
-        keys = db.fwmkeys()
         records = []
-        keys.each do |key|
-          metric_uuid = key.split('/').first
-          value = db.get(key)
-          records << deserialize(value, metric_uuid) if value
+        each_day(start_time, end_time) do |timestamp|
+          key = key(metric_uuid, timestamp)
+          values = db.get(key)
+          records << deserialize(values, metric_uuid) if values
         end
-        filter_records(records, query)
+        filter_records(records.flatten!, query)
       end
     end
 
     protected
 
-    def key(resource)
-      "#{resource.metric_uuid}/#{resource.timestamp.strftime("%j")}"
+    def each_day(from, to, &blk)
+      begin
+        yield from
+        from += (24 * 60 * 60)
+      end until from > to
+    end
+
+    def parse_query(query)
+      uuid = nil
+      start_time = end_time = Time.now
+
+      conditions = query.conditions
+      conditions.operands.each do |op|
+        if op.property.name == :metric_uuid
+          uuid = op.value
+        elsif op.property.name == :timestamp 
+          case op
+          when DataMapper::Conditions::EqualToComparison
+            start_time = end_time = op.value
+          when DataMapper::Conditions::InclusionComparison
+            start_time, end_time = op.value.begin, op.value.end
+          when DataMapper::Conditions::GreaterThanComparison,
+               DataMapper::Conditions::GreaterThanOrEqualToComparison
+            start_time = op.value
+
+          else
+            raise ArgumentError, "#{op.class.inspect} not supported"
+
+          end
+        else
+          raise ArgumentError, "Can't query on #{op.property.type.inspect}"
+        end
+      end
+
+      return uuid, start_time, end_time
+    end
+
+    def key(*args)
+      if args.first.is_a?(DataMapper::Resource)
+        resource = args.first
+        uuid, timestamp = resource.metric_uuid, resource.timestamp
+      else
+        uuid, timestamp = *args
+      end
+      "#{uuid}/#{timestamp.strftime("%Y%m%d")}"
     end
 
     def serialize(resource)
@@ -45,7 +87,7 @@ module DataMapper::Adapters
     def deserialize(string, metric_uuid)
       data = []
       data << string.slice!(0,8).unpack('If') until string.empty?
-      data.map { |d| {"timestamp" => d[0], "value" => d[1], "metric_uuid" => metric_uuid} }
+      data.map { |d| {"timestamp" => Time.at(d[0]), "value" => d[1], "metric_uuid" => metric_uuid} }
     end
 
     def save(db, key, value)
--- spec/dm-tiny-tt-adapter_spec.rb
+++ spec/dm-tiny-tt-adapter_spec.rb
@@ -4,6 +4,7 @@ require 'dm-core/spec/adapter_shared_spec'
 
 require 'uuidtools'
 require 'dm-types/uuid'
+require 'dm-core/core_ext/symbol'
 
 describe DataMapper::Adapters::TinyTtAdapter do
   before :all do
@@ -14,26 +15,119 @@ describe DataMapper::Adapters::TinyTtAdapter do
     class ::Observation
       include DataMapper::Resource
 
-      property :metric_uuid,  UUID,       :key => true
-      property :timestamp,    Time,       :key => true
+      property :metric_uuid,  UUID,  :key => true
+      property :timestamp,    Time,  :key => true
       property :value,        Float
 
+    end
+
+    @metric_uuid = UUID.random_create
+
+    @now = Time.at(Time.now.to_i) 
+    if (hour = @now.hour) > 12
+      # normalize things to noon, so if we run this late in the day,
+      # today's datapoints don't end up in tomorrow's bucket
+      @now -= (hour-12)*60*60
+    end
+  end
+
+  after :all do
+    # Clear out the DB
+    @adapter.db do |db|
+      db.vanish
     end
   end
 
   describe 'create' do
-    before :all do
-      @metric_uuid = UUID.random_create
-    end
 
-    it 'should do stuff' do
+    it 'should create a record' do
       Observation.create(:metric_uuid => @metric_uuid,
-                         :timestamp => Time.now,
+                         :timestamp => @now,
                          :value => 42.0)
 
+      Observation.get(@metric_uuid, @now).should_not be_nil
     end
 
   end
 
+  describe 'read' do
+    before :all do
+      @yest = @now - (24*60*60)
+      @tomm = @now + (24*60*60)
+      3.times do |i|
+        offset = i * 600
+        obs = Observation.create(:metric_uuid => @metric_uuid,
+                                 :timestamp => @yest + offset,
+                                 :value => offset.to_f)
+        instance_variable_set(:"@yest_#{i+1}", obs)
+
+        obs = Observation.create(:metric_uuid => @metric_uuid,
+                                 :timestamp => @now + offset,
+                                 :value => offset.to_f)
+        instance_variable_set(:"@today_#{i+1}", obs)
+
+        obs = Observation.create(:metric_uuid => @metric_uuid,
+                                 :timestamp => @tomm + offset,
+                                 :value => offset.to_f)
+        instance_variable_set(:"@tomm_#{i+1}", obs)
+      end
+
+    end
+
+    it 'should be able to retrieve datapoints' do
+      Observation.get(@metric_uuid, @now).should == @today_1
+    end
+
+    describe 'equal' do
+      before :all do
+        @result = Observation.all(:metric_uuid => @metric_uuid, :timestamp => @now)
+      end
+
+      it 'should return a single datapoint' do
+        @result.size.should == 1
+      end
+
+      it 'should return the datapoint that matches' do
+        @result.should include(@today_1)
+      end
+    end
+
+    describe 'range' do
+      before :all do
+        @result = Observation.all(:metric_uuid => @metric_uuid, :timestamp => (@yest..@now))
+      end
+
+      it 'should return the datapoints that match' do
+        [@yest_1, @yest_2, @yest_3, @today_1].each { |dp|
+          @result.should include(dp)
+        }
+      end
+
+      it 'should return the datapoints that match' do
+        [@today_2, @today_3, @tomm_1, @tomm_2, @tomm_3].each { |dp|
+          @result.should_not include(dp)
+        }
+      end
+    end
+
+    describe 'greater than or equal to' do
+      before :all do
+        @result = Observation.all(:metric_uuid => @metric_uuid, :timestamp.gte => @yest)
+      end
+
+      it 'should return the datapoints that match' do
+        [@yest_1, @yest_2, @yest_3, @today_1, @today_2, @today_3].each { |dp|
+          @result.should include(dp)
+        }
+      end
+
+      it 'should not include datapoints from after midnight tonight' do
+        [@tomm_1, @tomm_2, @tomm_3].each { |dp|
+          @result.should_not include(dp)
+        }
+      end
+    end
+
+  end
 
 end
